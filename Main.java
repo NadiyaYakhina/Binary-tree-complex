a230e13b5205a4603a9006cb52d72d04055affa0
NadiyaYakhina <55573290+NadiyaYakhina@users.noreply.github.com>
2022-11-25 08:54:57 +0100

    Binary tree plus sum of nodes and average

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-// class to create nodes
 class Node {
     int key;
     Node left;
@@ -13,54 +12,59 @@ class Node {
 
 class Main {
 
-  // additional variable - flag - which stores information whether the value 
-    // is found or not
-    // the flag is set in searchTree() method
     public static boolean foundFlag = false;
-
+    public static int sum = 0;
+    public static int counter = 0;
   
-  // Traverse search tree
-  public static void traverseTree(Node node, int value) {
-    if (node != null) {
-      traverseTree(node.left, value);
-      // System.out.print(" " + node.key);
-      if (node.key == value)
-            foundFlag = true;
-      traverseTree(node.right, value);
+    public static void traverseTree(Node node, int value) {
+        if (node != null) {
+            traverseTree(node.left, value);
+            if (node.key == value)
+                foundFlag = true;
+            traverseTree(node.right, value);
+        }
     }
-  }
 
-    
     public static void searchTree(Node node, int searchedValue) {
-        //Check whether the tree is empty
         if(node == null){
             System.out.println("Tree is empty");
         }
         else{
-            //If value is found in the given binary tree then, set 
-            //the flag to true
             if(node.key == searchedValue){
                 foundFlag = true;
                 return;
             }
-            //Search in left subtree
             if(foundFlag == false && node.left != null){
                 searchTree(node.left, searchedValue);
             }
-            //Search in right subtree
             if(foundFlag == false && node.right != null){
                 searchTree(node.right, searchedValue);
             }
         }
     }
+    
+    //Sum tree
+    public static int sumTree(Node node) {
+        if (node.left != null) { sumTree(node.left); }
+        sum += node.key;
+        if (node.right != null) { sumTree(node.right); }
+        return sum;
+    }
+    
+    //Mean tree
+    public static int meanTree(Node node) {
+        if (node.left != null) { meanTree(node.left); }
+        sum += node.key;
+        counter++;
+        if (node.right != null) { meanTree(node.right); }
+        return sum/counter;
+    }
 
 
 
     public static void main(String[] args) {
-        // create an node object
         Node root = new Node(10);
 
-        // create nodes of the tree
         root.left = new Node(2);
         root.left.left = new Node(4);
         root.left.right = new Node(7);
@@ -71,7 +75,6 @@ class Main {
         root.left.left.right = new Node(17);
         root.left.right.right = new Node(14);
         root.left.right.left = new Node(41);
-
         
         long startTime = System.currentTimeMillis();
         for(long i = 0; i < 100000000; i++){
@@ -91,11 +94,32 @@ class Main {
             }
         endTime = System.currentTimeMillis();
         System.out.println(foundFlag);
-        System.out.println("SearchTree: " + (endTime - startTime));   
+        System.out.println("SearchTree: " + (endTime - startTime)); 
         
-   
+        
+        startTime = System.currentTimeMillis();
+        for (long i=0; i<100000000;i++) {
+            sum = 0;
+            sumTree(root);
+        }
+        endTime = System.currentTimeMillis();
+        sum = 0;
+        System.out.println(sumTree(root));
+        System.out.println("SumTree: " + (endTime - startTime));
+        
+        startTime = System.currentTimeMillis();
+        for (long i=0; i<100000000;i++) {
+            sum = 0;
+            sumTree(root);
+        }
+        endTime = System.currentTimeMillis();
+        sum = 0;
+        counter = 0;
+        System.out.println(meanTree(root));
+        System.out.println("MeanTree: " + (endTime - startTime));
     }
 }
+
 
 
 
